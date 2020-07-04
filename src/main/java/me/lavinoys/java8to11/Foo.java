03e468e4907a045f94bf6f5761d85319f855d130
lavinoys <devjay0530@gmail.com>
2020-07-05 00:43:11 +0900

    익명함수없이 클래스를 함수형으로 만들수 있다.

--- src/main/java/me/lavinoys/java8to11/Foo.java
+++ src/main/java/me/lavinoys/java8to11/Foo.java
@@ -3,11 +3,7 @@ package me.lavinoys.java8to11;
 public class Foo {
 
     public static void main(String[] args) {
-        int baseNumber = 10;
-
-        RunSomething runSomething = number -> number + baseNumber;
-
-        System.out.println(runSomething.doIt(1));
-        System.out.println(runSomething.doIt(2));
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(1));
     }
 }
--- src/main/java/me/lavinoys/java8to11/RunSomething.java
+++
@@ -1,7 +0,0 @@
-package me.lavinoys.java8to11;
-
-@FunctionalInterface
-public interface RunSomething { // 수제로 만든 함수형 인터페이스
-
-    int doIt(int number);
-}
