12cd69e8e38a00898f0ae2904cacf1c5ae926b65
lavinoys <devjay0530@gmail.com>
2020-07-05 00:31:46 +0900

    이용하고 싶다면 변형이되는 값을 두고 이런식으로 해야함

--- src/main/java/me/lavinoys/java8to11/Foo.java
+++ src/main/java/me/lavinoys/java8to11/Foo.java
@@ -3,16 +3,11 @@ package me.lavinoys.java8to11;
 public class Foo {
 
     public static void main(String[] args) {
-        // 익명 내부 클래스
-        RunSomething runSomething = (number) -> {
-            return number + 10;
-        };
-        System.out.println(runSomething.doIt(1));
-        System.out.println(runSomething.doIt(1));
-        System.out.println(runSomething.doIt(1));
+        int baseNumber = 10;
 
-        System.out.println(runSomething.doIt(2));
-        System.out.println(runSomething.doIt(2));
+        RunSomething runSomething = number -> number + baseNumber;
+
+        System.out.println(runSomething.doIt(1));
         System.out.println(runSomething.doIt(2));
     }
 }
