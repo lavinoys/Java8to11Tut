72a74d3e7105cc79c610dde1ff6a44c2d42dfa5a
lavinoys <devjay0530@gmail.com>
2020-07-05 00:53:23 +0900

    무조건 10을 반환하는 함수

--- src/main/java/me/lavinoys/java8to11/Foo.java
+++ src/main/java/me/lavinoys/java8to11/Foo.java
@@ -1,17 +1,12 @@
 package me.lavinoys.java8to11;
 
-import java.util.function.Function;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Foo {
 
     public static void main(String[] args) {
-        Function<Integer, Integer> plus10 = (i) -> i + 10;
-        Function<Integer, Integer> multiply2 = (i) -> i * 2;
-
-
-        // Higher-Order Function
-        // 고차원 함수
-        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
-        System.out.println(multiply2AndPlus10.apply(2));
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10);
     }
 }
